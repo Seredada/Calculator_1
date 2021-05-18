3d646750f16ebc43f854f9761470119ef0569bb9
Seredada <antoser74@gmail.com>
2021-05-18 14:59:58 +0300

    Простенький калькулятор, учусь.

---
+++ src/Calc.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Calc {
+    public void calcul() {
+
+        double rezul;
+        double operand1;
+        double operand2;
+        char operation;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите первуй цифру: ");
+        operand1 = scanner.nextDouble();
+        System.out.print("Введите действие + - * /: ");
+        operation = scanner.next().charAt(0);
+        System.out.print("Введите вторую цифру: ");
+        operand2 = scanner.nextDouble();
+
+        switch (operation){
+            case '+': rezul = operand1 + operand2;
+                break;
+
+            case '-': rezul = operand1 - operand2;
+                break;
+
+            case '*': rezul = operand1 * operand2;
+                break;
+
+            case '/': rezul = operand1 / operand2;
+                break;
+
+            default:
+                System.out.println("Что-то неправильно ввел");
+                return;
+        }
+
+        System.out.print( operand1 + " " + operation + " " + operand2 + " = ");
+
+        System.out.printf("%.3f",rezul);
+
+    }
+
+}
+
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+// Учусь делать простенький калькулятор
+public class Main {
+
+    public static void main(String[] args) {
+
+        Calc rez = new Calc();
+        rez.calcul();
+
+    }
+
+}
+
+
+
